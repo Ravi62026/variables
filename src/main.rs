fdb82473abb2e84d19e24bd2328bf84ec168c81f
Ravi <rs1806105@gmail.com>
2024-11-18 19:32:01 +0530

    variable

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let age = 21;
+    println!("My age is {age}");
+
+    // age = 34;
+
+    // constants
+
+    const PI:f64 = 3.14;
+    println!("{PI}");
+
+    const EXTRA : u32= 32;
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60*60*3 + EXTRA;
+
+    println!("THREE_HOURS_IN_SECONDS with extra is: {THREE_HOURS_IN_SECONDS}")
+
+
+
+}
